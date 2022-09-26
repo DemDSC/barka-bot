bd2394a439245cbdbf9dcd15d4b7ac7d05310979
Sebastian Tkocz <regers9876@gmail.com>
2022-09-26 22:36:38 +0200

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+    client.user.setActivity("Tera sie w grobie obraca");
+});
+
+// Login to Discord with your client's token
+client.login(token);
